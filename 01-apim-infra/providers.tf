8d2fd0c2cb7c452625d08b01d76015a9aa61c630
nexgbitslabs <nexbitslabs@nexgbits.com>
2025-05-26 15:00:02 -0400

    updated

--- 01-apim-infra/providers.tf
+++ 01-apim-infra/providers.tf
@@ -1,7 +1,16 @@
-output "resource_group_name" {
-  value = var.rg_name
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
 }
-
-output "api_management_service_name" {
-  value = azurerm_api_management.api.name
+provider "azurerm" {
+  features {}
 }
